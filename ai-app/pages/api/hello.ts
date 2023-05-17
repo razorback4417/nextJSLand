92501e389f2437b04de4ed99d1c8c2fe70c36443
razorback4417 <razorback4417.800@gmail.com>
2023-05-17 13:54:36 -0700

    api test integration

--- ai-app/pages/api/hello.ts
+++ ai-app/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import openai from '@/utils/openai';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "What is 2+2",
+  });
+  const responseText = completion.data.choices[0].text;
+  res.status(200).json({responseText})
+  //receieve some user data, ask ChatGPT to generate something and return to the user
+
 }
